f5b0e9818635acfcc8397d1225554e9d06dd34dd
Florian Kranz <florianmaximiliankranz@gmail.com>
2023-12-22 19:58:01 +0100

    ✨ Introducing the capspam mosule (Issue #4)

--- gitposter_bot.py
+++ gitposter_bot.py
@@ -3,6 +3,8 @@ import os
 # telegram bot library
 import telebot
 
+import modules.gp_capspam as gp_capspam
+
 # create bot with imported token
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -24,6 +26,19 @@ def send_insult(message):
 @bot.message_handler(commands=['t_sticker'])
 def gp_send_sticker(message):
     bot.send_sticker(message.chat.id, sticker='CAACAgIAAxkBAAEoW6plgto1t_V5gcGgCZtQX1BgkxH7HQACKDkAAqsKSEpQRYmZhRKHxDME')
+
+
+# capspam call
+@bot.message_handler()
+def call_gp_capspam(*messages):
+    for message in messages:
+        print(message.text)
+        capspam_result = gp_capspam.get_capspam_result(message)
+        if(capspam_result != ""):
+            for character in capspam_result:
+                character = character
+                bot.send_message(message.chat.id, character)
+
 
 # start bot listening
 bot.infinity_polling()
